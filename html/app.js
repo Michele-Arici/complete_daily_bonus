54a0ec88533ef98374e6719e1c29431266832c4a
Michele <miki.arici8@gmail.com>
2023-12-28 18:04:55 +0100

    svg

--- html/app.js
+++ html/app.js
@@ -134,6 +134,8 @@ const app = Vue.createApp({
             return itemDiv;
         },
         initializeRoulette() {
+            //const svg = `<svg class="" version="1.1" xmlns="http://www.w3.org/2000/svg" width="174" height="200" viewBox="0 0 173.20508075688772 200" style="/* filter: drop-shadow(rgba(255, 255, 255, 0.5) 0px 0px 10px); */height: 10rem;width: 9rem;margin: 3px;/* border-radius: 15px; *//* border-bottom: 3px solid rgba(0, 0, 0, 0.2); */display: flex;flex-wrap: nowrap !important;align-items: center;justify-content: center;color: white;font-size: 1.5em;/* border: 3px solid #ffffff4a; */" stroke="#ffffff4a" stroke-width="8px"><path fill="#fff" d="M73.61215932167728 7.499999999999999Q86.60254037844386 0 99.59292143521044 7.499999999999999L160.21469970012114 42.5Q173.20508075688772 50 173.20508075688772 65L173.20508075688772 135Q173.20508075688772 150 160.21469970012114 157.5L99.59292143521044 192.5Q86.60254037844386 200 73.61215932167728 192.5L12.99038105676658 157.5Q0 150 0 135L0 65Q0 50 12.99038105676658 42.5Z"></path></svg>`
+
             var data = this.rouletteData;
 
             const numCopies = this.rouletteData.length * 2;
